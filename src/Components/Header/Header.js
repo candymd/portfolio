18e4a35adcef15703b2a58068073b43d9ba4af94
Candy <candymdeoleo@gmail.com>
2022-03-04 23:23:31 +0100

    inicial commit, added header and hero

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+const Header = () => {
+
+    return (
+    <header className="header">
+        <nav>
+        <ul>
+            <li>Home</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        </nav>
+    </header>
+
+        )}
+
+export default Header
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,33 @@
+import "./Hero.css"
+import {github, linkedin, mail} from "../../assets/socialMedia-icons/socialMedia-icons"
+import illustration from "../../assets/illustration.svg"
+
+const Hero = () => {
+    return (
+        <main className="hero">
+        <div className="intro-text">
+            <h1>
+                Hello, I am Candy Montero, a barcelona-based web developer.
+            </h1>
+
+            <div className="socialMedia-icons">
+                    <a href="https://linkedin.com/in/candymd" target="_blank" rel="noreferrer">
+                        <img alt="github icon" src={linkedin}/>
+                    </a>
+                <a href="https://github.com/candymd" target="_blank" rel="noreferrer">
+                    <img alt="linkedin icon" src={github}/>
+                </a>
+                <a href="mailto:candymdeoleo@gmail.com" target="_blank" rel="noreferrer">
+                    <img alt="mail icon" src={mail}/>
+                </a>
+            </div>
+        </div>
+            <div className="illustration">
+                <img alt="computer, plant and coffee" src={illustration}/>
+            </div>
+        </main>
+    )
+
+}
+
+export default Hero
