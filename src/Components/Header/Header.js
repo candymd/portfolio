ad8efa1dba438852f8a8d0c9189e775244ffa9e9
Candy <candymdeoleo@gmail.com>
2022-03-08 15:08:20 +0100

    different tone background color on hover link header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><Link className="link" activeClass="active" to="hero" spy={true} smooth={true} duration={500}>Home</Link></li>
-                    <li><Link className="link" activeClass="active" to="skills" spy={true} smooth={true}  duration={500}>Skills</Link></li>
-                    <li><Link className="link" activeClass="active" to="projects" spy={true} smooth={true}  duration={500}>Projects</Link></li>
-                    <li><Link className="link" activeClass="active" to="contact" spy={true} smooth={true}  duration={500}>Contact</Link></li>
+                    <li><Link className="link" activeClass="active" to="skills" offset={-15}  spy={true} smooth={true}  duration={500}>Skills</Link></li>
+                    <li><Link className="link" activeClass="active" to="projects" offset={-15} spy={true} smooth={true}  duration={500}>Projects</Link></li>
+                    <li><Link className="link" activeClass="active" to="contact" offset={-15} spy={true} smooth={true}  duration={500}>Contact</Link></li>
                 </ul>
             </nav>
         </header>
