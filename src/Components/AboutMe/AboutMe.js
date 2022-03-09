23956181e0b4d2ed15a5e267ae35ac9dfbfe0774
Candy <candymdeoleo@gmail.com>
2022-03-09 16:38:54 +0100

    added about me text

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -6,12 +6,12 @@ const AboutMe = () => {
       <section className="aboutMe section">
      <h1>About me</h1>
           <div className="description-and-image">
-              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. <br/><br/>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                  laborum."</p>
+              <p><q>I'd always consider myself a techie and someone who's very passionate about technology.
+                  In 2021 I joined FactoriaF5's Femtech 
+                  bootcamp where I trained to be become a fullstack developer by making projects using technologies such as ReactJs, Spring, Github and working with agile methodology.
+                   <br/><br/>
+                  Apart from coding, one of my main interests is social justice. I would love to take part in 
+                  projects which focus on things like diversity and inclusion, especially for women in tech.</q></p>
               <img src={homeOffice} alt={homeOffice + 'illustration'} />
           </div>
       </section>
