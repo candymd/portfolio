132b6ce746163e565332c05996c1c9f8d8c872d9
Candy <candymdeoleo@gmail.com>
2022-03-15 18:42:12 +0100

    added some test

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,60 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import AboutMe from './Components/AboutMe/AboutMe';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
 import Projects from './Components/Projects/Projects';
 import Skills from './Components/Skills/Skills';
 
-test('renders learn react link', () => {
+test('renders component App', () => {
   const app = render(<App />);
-  const hero = render(<Hero />);
-  const projects = render(<Projects />);
-  const skills = render(<Skills />);
-  const contact = render(<Contact />);
-  // expect(app).toContain(hero, projects, skills, contact);
-  console.log(app);
+ 
+  expect(app).toBeInTheDocument;
 });
+
+test('renders component Header', () => {
+  const header = render(<Header />);
+ 
+  expect(header).toBeInTheDocument;
+});
+
+test('renders component AboutMe', () => {
+  const aboutMe = render(<AboutMe />);
+ 
+  expect(aboutMe).toBeInTheDocument;
+});
+
+test('renders component Contact', () => {
+  const contact = render(<Contact />);
+ 
+  expect(contact).toBeInTheDocument;
+});
+
+test('renders component Hero', () => {
+  const hero = render(<Hero />);
+ 
+  expect(hero).toBeInTheDocument;
+});
+
+test('renders component Footer', () => {
+  const footer = render(<Footer />);
+ 
+  expect(footer).toBeInTheDocument;
+});
+
+test('renders component Projects', () => {
+  const projects = render(<Projects />);
+ 
+  expect(projects).toBeInTheDocument;
+});
+
+test('renders component Skills', () => {
+  const skills = render(<Skills />);
+ 
+  expect(skills).toBeInTheDocument;
+});
+
+
 
